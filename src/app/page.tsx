82aeabf25e65513709505b8bde5f4a6a2af19ca2
TuVoXuan <voxuantu1235@gmail.com>
2025-02-10 15:45:27 +0700

    feat: downgrade tailwind v4 to v3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <div className="text-blue-500 text-desktop-lg">DM Serif Display </div>
-      <p className="font-onset text-success-400">
+      <p className="text-success-400 text-body-lg">
         As a product designer, one's primary focus is to create user-centered designs that are both functional and
         aesthetically pleasing.
       </p>
